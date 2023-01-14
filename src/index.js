a933b887aae52f6a4596ade34b87f110a668b2af
Nikolay <116117451+psiho228@users.noreply.github.com>
2023-01-14 13:30:19 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
-function toReadable {
-  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-              'seventeen', 'eighteen', 'nineteen'];
-  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-              'ninety'];
+var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 
-  var numString = num.toString();
-
-  if (num < 0) throw new Error('Negative numbers are not supported.');
-
-  if (num === 0) return 'zero';
-
-  //the case of 1 - 20
-  if (num < 20) {
-    return ones[num];
-  }
-
-  if (numString.length === 2) {
-    return tens[numString[0]] + ' ' + ones[numString[1]];
-  }
-
-  //100 and more
-  if (numString.length == 3) {
-    if (numString[1] === '0' && numString[2] === '0')
-      return ones[numString[0]] + ' hundred';
-    else
-      return ones[numString[0]] + ' hundred and ' + convert(+(numString[1] + numString[2]));
-  }
-
-  if (numString.length === 4) {
-    var end = +(numString[1] + numString[2] + numString[3]);
-    if (end === 0) return ones[numString[0]] + ' thousand';
-    if (end < 100) return ones[numString[0]] + ' thousand and ' + convert(end);
-    return ones[numString[0]] + ' thousand ' + convert(end);
-  }
+function toReadable{
+    if (n < 20) return num[n];
+    var digit = n%10;
+    if (n < 100) return tens[~~(n/10)-2] + (digit? "-" + num[digit]: "");
+    if (n < 1000) return num[~~(n/100)] +" hundred" + (n%100 == 0? "": " and " + number2words(n%100));
+    return number2words(~~(n/1000)) + " thousand" + (n%1000 != 0? " " + number2words(n%1000): "");
 }
