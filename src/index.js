ac3518adaf18d00c12fb4abb17c694708ef28e21
Nikolay <116117451+psiho228@users.noreply.github.com>
2023-01-14 13:22:51 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,79 +1,37 @@
-   function toReadable {
-            var words = new Array();
-            words[0] = '';
-            words[1] = 'One';
-            words[2] = 'Two';
-            words[3] = 'Three';
-            words[4] = 'Four';
-            words[5] = 'Five';
-            words[6] = 'Six';
-            words[7] = 'Seven';
-            words[8] = 'Eight';
-            words[9] = 'Nine';
-            words[10] = 'Ten';
-            words[11] = 'Eleven';
-            words[12] = 'Twelve';
-            words[13] = 'Thirteen';
-            words[14] = 'Fourteen';
-            words[15] = 'Fifteen';
-            words[16] = 'Sixteen';
-            words[17] = 'Seventeen';
-            words[18] = 'Eighteen';
-            words[19] = 'Nineteen';
-            words[20] = 'Twenty';
-            words[30] = 'Thirty';
-            words[40] = 'Forty';
-            words[50] = 'Fifty';
-            words[60] = 'Sixty';
-            words[70] = 'Seventy';
-            words[80] = 'Eighty';
-            words[90] = 'Ninety';
-            number = number.toString();
-            var atemp = number.split(".");
-            var number = atemp[0].split(",").join("");
-            var n_length = number.length;
-            var words_string = "";
-            if (n_length <= 9) {
-                var n_array = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
-                var received_n_array = new Array();
-                for (var i = 0; i <n_length; i++) {
-                    received_n_array[i] = number.substr(i, 1);
-                }
-                for (vari =9 - n_length,j =0; i < 9; i++ , j++) {
-                    n_array[i] = received_n_array[j];
-                }
-                for (vari =0,j =1; i < 9; i++ , j++) {
-                    if (i == 0 || i == 2 || i == 4 || i == 7) {
-                        if (n_array[i] == 1) {
-                            n_array[j] = 10 + parseInt(n_array[j]);
-                            n_array[i] = 0;
-                        }
-                    }
-                }
-                value ="";
-                for (vari =0; i < 9; i++) {
-                    if (i == 0 || i == 2 || i == 4 || i == 7) {
-                        value =n_array[i] * 10;
-                    } else {
-                        value =n_array[i];
-                    }
-                    if (value != 0) {
-                        words_string += words[value] + " ";
-                    }
-                    if ((i == 1 && value != 0) || (i == 0 && value != 0 && n_array[i + 1] == 0)) {
-                        words_string += "Crores ";
-                    }
-                    if ((i == 3 && value != 0) || (i == 2 && value != 0 && n_array[i + 1] == 0)) {
-                        words_string += "Lakhs ";
-                    }
-                    if ((i == 5 && value != 0) || (i == 4 && value != 0 && n_array[i + 1] == 0)) {
-                        words_string += "Thousand ";
-                    }
-                    if (i == 6 && value != 0 && (n_array[i + 1] != 0 && n_array[i + 2] != 0)) {
-                        words_string += "Hundred and ";
-                    } else if (i == 6 && value != 0) {
-                        words_string += "Hundred ";
-                    }
-                }
-                words_string = words_string.split("  ").join(" ");
-            }
+   function toReadable{
+  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+              'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+              'ninety'];
+
+  var numString = num.toString();
+
+  if (num < 0) throw new Error('Negative numbers are not supported.');
+
+  if (num === 0) return 'zero';
+
+  //the case of 1 - 20
+  if (num < 20) {
+    return ones[num];
+  }
+
+  if (numString.length === 2) {
+    return tens[numString[0]] + ' ' + ones[numString[1]];
+  }
+
+  //100 and more
+  if (numString.length == 3) {
+    if (numString[1] === '0' && numString[2] === '0')
+      return ones[numString[0]] + ' hundred';
+    else
+      return ones[numString[0]] + ' hundred and ' + convert(+(numString[1] + numString[2]));
+  }
+
+  if (numString.length === 4) {
+    var end = +(numString[1] + numString[2] + numString[3]);
+    if (end === 0) return ones[numString[0]] + ' thousand';
+    if (end < 100) return ones[numString[0]] + ' thousand and ' + convert(end);
+    return ones[numString[0]] + ' thousand ' + convert(end);
+  }
+}
